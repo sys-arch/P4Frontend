baf6ee7f03049a01a62791cdcdb394169637f214
adriangm16 <96481313+adriangm16@users.noreply.github.com>
2024-10-07 20:32:54 +0200

    Frontend ventana registro
    No esta completo falta alguna compración

--- Reuneme/src/app/registro/registro.component.ts
+++ Reuneme/src/app/registro/registro.component.ts
@@ -1,12 +1,74 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [],
   templateUrl: './registro.component.html',
-  styleUrl: './registro.component.css'
+  styleUrls: ['./registro.component.css'],
+  imports: [FormsModule, CommonModule],
+  providers: [UserService]
 })
 export class RegistroComponent {
+  username: string = '';
+  password1: string = '';
+  password2: string = '';
+  confirmPassword: string = '';
+  passwordError: string = '';
+  confirmPasswordError: string = '';
+  passwordVisible = false;
+  emailInvalid = false;
+  loginFailed = false;
 
+  constructor(
+    private router: Router,
+    private userService: UserService,
+    private cdr: ChangeDetectorRef
+  ) {} 
+
+
+  validateEmail(): void {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    this.emailInvalid = !emailPattern.test(this.username);
+  }
+
+  // Comprobación del formato de la contraseña
+  validarPassword(): boolean {
+    const passwordRegEx = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    if (this.password1.match(passwordRegEx)) {
+      this.passwordError = '';
+      return true;
+    } else {
+      this.passwordError = 'La contraseña debe tener al menos 8 caracteres, una letra mayúscula, un número y un carácter especial';
+      return false;
+    } 
+  }
+
+  validarConfirmPassword(): boolean {
+    if (this.password1 === this.password2) {
+      this.confirmPasswordError = '';
+      return true;
+    } else {
+      this.confirmPasswordError = 'Las contraseñas no coinciden';
+      return false;
+    }
+  }
+
+  togglePasswordVisibility(): void {
+    this.passwordVisible = !this.passwordVisible;
+    const passwordInput = document.getElementById('password') as HTMLInputElement;
+    passwordInput.type = this.passwordVisible ? 'text' : 'password';
+  }
+
+  goToLanding(): void {
+    this.router.navigate(['/']); 
+  }
+
+  goToLogin(): void {
+    this.router.navigate(['/login']); 
+  }
 }
