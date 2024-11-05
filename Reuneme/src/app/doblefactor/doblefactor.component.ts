c319a50b5f909de544f77368c2d0239b244f40ce
LuisFernandezMedina <luiseduardo.fdez@alu.uclm.es>
2024-11-05 12:13:47 +0100

    Doble factor, sin qr

--- Reuneme/src/app/doblefactor/doblefactor.component.ts
+++ Reuneme/src/app/doblefactor/doblefactor.component.ts
@@ -1,14 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoaderComponent } from "../loader/loader.component";
-import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../shared/footer/footer.component';
+import { HeaderComponent } from '../shared/header/header.component';
 
 @Component({
   selector: 'app-doblefactor',
   standalone: true,
   templateUrl: './doblefactor.component.html',
   styleUrl: './doblefactor.component.css',
-  imports: [CommonModule,LoaderComponent]
+  imports: [CommonModule,LoaderComponent,HeaderComponent, FooterComponent]
 })
 export class DoblefactorComponent {
   isLoading: boolean = false;
