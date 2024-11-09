189a8c932c7df395939aa3e4929c1d718db95bfa
AntonioSanSan <antonio.sanchez36@alu.uclm.es>
2024-11-09 13:48:53 +0100

    Reorganizacion lista usuarios

--- Reuneme/src/app/shared/menu/menu.component.ts
+++ Reuneme/src/app/shared/menu/menu.component.ts
@@ -8,7 +8,8 @@ import { TurnosHorariosComponent } from '../turnos-horarios/turnos-horarios.comp
   standalone: true,
   imports: [ListaUsuariosComponent, AusenciasComponent, TurnosHorariosComponent],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+  styleUrl: './menu.component.css',
+
 })
 
 export class MenuComponent {
@@ -18,9 +19,8 @@ export class MenuComponent {
   // Método para cambiar de pestaña
   mostrarPestana(pestana: string) {
     this.pestanaActual = pestana;
-  }
-
-  // Método para verificar si una pestaña está activa
+  }  
+    // Método para verificar si una pestaña está activa
   isPestanaActiva(pestana: string): boolean {
     return this.pestanaActual === pestana;
   }
--- Reuneme/src/app/ventana-principal/ventana-principal.component.ts
+++ Reuneme/src/app/ventana-principal/ventana-principal.component.ts
@@ -10,11 +10,13 @@ import { CalendarioComponent } from '../shared/calendario/calendario.component';
 import { FooterComponent } from '../shared/footer/footer.component';
 import { HeaderComponent } from '../shared/header/header.component';
 import { MenuComponent } from '../shared/menu/menu.component';
+import { ListaUsuariosComponent } from '../shared/lista-usuarios/lista-usuarios.component';
+
 
 @Component({
   selector: 'app-ventana-principal',
   standalone: true,
-  imports: [CommonModule, FormsModule, LoaderComponent, FooterComponent, HeaderComponent,CalendarioComponent, MenuComponent, AusenciasComponent],
+  imports: [CommonModule, FormsModule, LoaderComponent, FooterComponent, HeaderComponent, CalendarioComponent, MenuComponent, AusenciasComponent, ListaUsuariosComponent],
   templateUrl: './ventana-principal.component.html',
   styleUrls: ['./ventana-principal.component.css']
 })
@@ -24,12 +26,7 @@ export class VentanaPrincipalComponent implements OnInit {
   token: string = '';
   myemail: string = '';
   isLoading: boolean = false;
-  searchBy: string = 'name';
-  searchQuery: string = '';
-  filterBy: string = 'all';
   selectedUser: any = null;
-  showBlockModal: boolean = false;
-  showValiModal: boolean = false;
   showDeleteModal: boolean = false;
   countdown: number = 5;
   countdownInterval: any;
@@ -42,6 +39,7 @@ export class VentanaPrincipalComponent implements OnInit {
     role: 'admin'
   };
 
+  /*ESTO QUIZAS HAYA Q QUITARLO */
   users = [
     {
       id: '1',
@@ -129,6 +127,8 @@ export class VentanaPrincipalComponent implements OnInit {
       }
     }
 
+    
+
     // Cargar ausencias desde localStorage al inicializar el componente
     const ausenciasGuardadas = localStorage.getItem('ausencias');
     if (ausenciasGuardadas) {
@@ -173,176 +173,6 @@ export class VentanaPrincipalComponent implements OnInit {
     this.activeTab = tab;
   }
 
-  toggleDelete(user: any): void {
-    this.selectedUser = user;
-    this.showDeleteModal = true;
-    this.countdown = 5;
-    this.startCountdown();
-  }
-
-  startCountdown(): void {
-    this.countdownInterval = setInterval(() => {
-      if (this.countdown > 0) {
-        this.countdown--;
-      } else {
-        clearInterval(this.countdownInterval);
-      }
-    }, 1000);
-  }
-
-  confirmDelete(): void {
-    if (this.selectedUser) {
-        this.userService.deleteUserByEmail(this.selectedUser.email, this.token).subscribe(
-            () => {
-                this.showDeleteModal = false; 
-                this.selectedUser = null; 
-                clearInterval(this.countdownInterval); 
-                this.loadAllUsers(); // Recarga la lista completa
-            },
-            (error) => {
-                console.error('Error al eliminar el usuario:', error);
-            }
-        );
-    }
-}
-
-loadAllUsers(): void {
-  this.isLoading = true;
-  this.users = []; // Limpia la lista antes de recargar
-
-  this.userService.getAllUsers(this.token).subscribe(
-    (userList: any[]) => {
-      console.log('Lista de usuarios:', userList);
-      this.users = userList.map(userInfo => {
-        const isAdmin = userInfo.hasOwnProperty('interno') && userInfo.interno !== undefined;
-        const isBlocked = !isAdmin && userInfo.bloqueado === true;
-        
-        return {
-          firstName: userInfo.nombre,
-          lastName: `${userInfo.apellido1} ${userInfo.apellido2}`,
-          email: userInfo.email,
-          isAdmin: isAdmin,
-          profilePicture: this.gravatarService.getGravatarUrl(userInfo.email),
-          estado: isAdmin ? 'Validado' : (isBlocked ? 'Bloqueado' : (userInfo.verificado ? 'Validado' : 'No validado')),
-        };
-      });
-      this.users.sort((a, b) => a.lastName.localeCompare(b.lastName));
-
-      this.isLoading = false;
-    },
-    (error) => {
-      console.error('Error al obtener la lista de usuarios:', error);
-      this.isLoading = false;
-    }
-  );
-}
-
-  cancelDelete(): void {
-    this.selectedUser = null;
-    this.showDeleteModal = false;
-    clearInterval(this.countdownInterval);
-  }
-
-  toggleBlocked(user: any): void {
-    if (user.isAdmin) {
-        alert("No se permite bloquear a un administrador.");
-        return;
-    }
-
-    if (user.estado === 'Bloqueado') {
-        // Desbloquea directamente si el usuario ya está bloqueado
-        this.userService.blockUserByEmail(user.email, false, this.token).subscribe(
-            () => {
-                user.estado = 'No validado'; // Actualiza el estado a "No validado" al desbloquear
-                this.loadAllUsers(); // Recarga la lista de usuarios
-            },
-            (error) => {
-                console.error('Error al desbloquear el usuario:', error);
-            }
-        );
-    } else {
-        // Si está desbloqueado, mostrar el modal de confirmación de bloqueo
-        this.selectedUser = user;
-        this.showBlockModal = true;
-    }
-  }
-
-  confirmBlock(): void {
-      if (this.selectedUser) {
-          // Determinar si se va a bloquear o desbloquear basado en el estado actual
-          const bloquear = this.selectedUser.estado !== 'Bloqueado';
-          
-          // Llamar al servicio para bloquear o desbloquear al usuario
-          this.userService.blockUserByEmail(this.selectedUser.email, bloquear, this.token).subscribe(
-              () => {
-                  // Actualizar el estado del usuario en la lista según el resultado
-                  this.selectedUser.estado = bloquear ? 'Bloqueado' : 'No validado';
-                  this.showBlockModal = false; // Cerrar el modal
-                  this.selectedUser = null; // Restablecer usuario seleccionado
-                  this.loadAllUsers(); // Recargar la lista de usuarios
-              },
-              (error) => {
-                  console.error('Error al cambiar el estado de bloqueo del usuario:', error);
-              }
-          );
-      }
-  }
-
-  cancelBlock(): void {
-      this.selectedUser = null;
-      this.showBlockModal = false;
-  }
-
-  toggleValidation(user: any): void {
-    if (user.estado === 'No validado') {
-      this.selectedUser = user;
-      this.showValiModal = true;
-    }
-  }
-
-  confirmValidation(): void {
-    if (this.selectedUser) {
-    
-      this.userService.verifyUserByEmail(this.selectedUser.email, this.token).subscribe(
-        () => {
-          this.selectedUser.estado = 'Validado';
-          this.showValiModal = false;
-          this.selectedUser = null;
-          this.loadAllUsers(); // Recarga la lista completa de usuarios
-        },
-        (error) => {
-          console.error('Error al verificar el usuario:', error);
-        }
-      );
-    }
-  }
-  
-  cancelValidation(): void {
-    this.selectedUser = null;
-    this.showValiModal = false;
-  }
-
-  filteredUsers() {
-    return this.users
-      .filter(user => {
-        if (this.filterBy === 'admin' && !user.isAdmin) return false;
-        if (this.filterBy === 'employee' && user.isAdmin) return false;
-        if (this.filterBy === 'blocked' && user.estado !== 'Bloqueado') return false;
-        if (this.filterBy === 'validated' && user.estado !== 'Validado') return false;
-        if (this.filterBy === 'notValidated' && user.estado !== 'No validado') return false;
-        return true;
-      })
-      .filter(user => {
-        const searchQueryLower = this.searchQuery.toLowerCase();
-        if (this.searchBy === 'name') {
-          return user.firstName.toLowerCase().includes(searchQueryLower);
-        } else if (this.searchBy === 'email') {
-          return user.email.toLowerCase().includes(searchQueryLower);
-        }
-        return false;
-      });
-  }
-
   // Método para navegar a una ruta específica
   navigateTo(route: string): void {
     this.isLoading = true;
@@ -352,16 +182,6 @@ loadAllUsers(): void {
     }, 1000);
   }
 
-  // Método para navegar a la vista de edición de usuario
-  editUser(user: any): void {
-    if (user) {
-      this.router.navigate(['/edicion-usuario'], { state: { user } });
-
-    } else {
-      console.error('El correo electrónico del usuario no está definido');
-    }
-  }
-  
   /*---------------------------------- Turnos Horarios ----------------------------------*/
   turnosHorarios: { inicio: number; fin: number; texto: string }[] = [
     {
@@ -544,6 +364,42 @@ loadAllUsers(): void {
   cancelDeleteAusencia(): void {
     this.ausenciaAEliminar = null;
     this.showDeleteModalAusencia = false;
+  }
+
+
+
+
+
+  // Cargar todos los usuarios
+  loadAllUsers(): void {
+    this.isLoading = true;
+    this.users = []; // Limpia la lista antes de recargar
+  
+    this.userService.getAllUsers(this.token).subscribe(
+      (userList: any[]) => {
+        console.log('Lista de usuarios:', userList);
+        this.users = userList.map(userInfo => {
+          const isAdmin = userInfo.hasOwnProperty('interno') && userInfo.interno !== undefined;
+          const isBlocked = !isAdmin && userInfo.bloqueado === true;
+          
+          return {
+            firstName: userInfo.nombre,
+            lastName: `${userInfo.apellido1} ${userInfo.apellido2}`,
+            email: userInfo.email,
+            isAdmin: isAdmin,
+            profilePicture: this.gravatarService.getGravatarUrl(userInfo.email),
+            estado: isAdmin ? 'Validado' : (isBlocked ? 'Bloqueado' : (userInfo.verificado ? 'Validado' : 'No validado')),
+          };
+        });
+        this.users.sort((a, b) => a.lastName.localeCompare(b.lastName));
+  
+        this.isLoading = false;
+      },
+      (error) => {
+        console.error('Error al obtener la lista de usuarios:', error);
+        this.isLoading = false;
+      }
+    );
   }
 
 }
