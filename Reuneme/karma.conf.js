1176296d14ee0f10fff825813d63a7f6ef1f6333
YolandaGR13 <yolanda.galvan@alu.uclm.es>
2024-11-24 15:57:54 +0100

    pruebas

--- Reuneme/karma.conf.js
+++ Reuneme/karma.conf.js
@@ -5,25 +5,19 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-coverage'),
-      require('karma-jasmine-html-reporter'), // Necesario para la interfaz gráfica
       require('@angular-devkit/build-angular/plugins/karma'),
     ],
-    reporters: ['progress', 'kjhtml', 'coverage'], // Añadir 'kjhtml' para el visor en navegador
+    reporters: ['progress', 'coverage'],
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage'),
+      dir: require('path').join(__dirname, './coverage/reuneme'),
       subdir: '.',
       reporters: [
-        { type: 'html' },
-        { type: 'text-summary' },
-        { type: 'lcovonly' },
+        { type: 'html' }, // Informe en HTML
+        { type: 'lcovonly' }, // Informe lcov.info para SonarQube
+        { type: 'text-summary' }, // Resumen en consola
       ],
-      fixWebpackSourcePaths: true,
     },
-    browsers: ['Chrome'], // Cambia a 'Chrome' para abrir el navegador
-    singleRun: false, // Mantén las pruebas corriendo para observar cambios
-    restartOnFileChange: true, // Reinicia automáticamente al cambiar archivos
-    client: {
-      clearContext: false, // Mantén la interfaz de pruebas visible en el navegador
-    },
+    browsers: ['ChromeHeadless'], // Usa el navegador sin interfaz gráfica
+    singleRun: true, // Cierra Karma automáticamente al finalizar
   });
 };
